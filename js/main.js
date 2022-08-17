6bcfa84600df9b5499451482101caee703cd8fa6
junya-yachi <109054387+junya-yachi@users.noreply.github.com>
2022-08-17 15:12:36 +0900

    電卓課題の提出です。

--- js/main.js
+++ js/main.js
@@ -1,72 +1,57 @@
 'use strict'
-{
-   $(document).ready(function() {
-    
-      let startTime;
-      let timeoutId;
-      let elapsedTime = 0;
 
-      function countUp() {
-        const d = new Date(Date.now() - startTime + elapsedTime);
-        const h = d.getUTCHours();
-        const m = d.getMinutes();
-        const s = d.getSeconds();
-        const ms = d.getMilliseconds();
-        $(".timer").text(`${h}:${m}:${s}:${ms}`);
-        
-        timeoutId = setTimeout(() => {
-          countUp();
-        }, 10);
-      }
+let $result = $(".result").innerHTML;
+$result = '';
 
-      function buttonDefault() {
-        $(".start").prop("disabled", false);
-        $(".stop").prop("disabled", true);
-        $(".reset").prop("disabled", true);
-      }
+function displayResult() {
+  $(".result").text(`${$result}`);
+}
 
-      function clickButtonStart() {
-        $(".start").prop("disabled", true);
-        $(".stop").prop("disabled", false);
-        $(".reset").prop("disabled", true);
-      }
+function clickButton(button) {
+  let number = button.innerHTML;
+  if($result === '0' || $result === '00') {
+    $result = number;
+    displayResult();
+  } else {
+    $result += number;
+    displayResult();
+    console.log($result);
+  }
+}
 
-      function clickButtonStop() {
-        $(".start").prop("disabled", false);
-        $(".stop").prop("disabled", true);
-        $(".reset").prop("disabled", false);
-      }
+function clickOperator(button) {
+  let number = button.innerHTML;
+  if ($result.slice(-1) === '+' || $result.slice(-1) === '-' || $result.slice(-1) === '*' || $result.slice(-1) === '/' || $result.slice(-1) === '') {
+    return;
+  }  else {
+    $result += number;
+    displayResult();
+    console.log($result);
+  } 
+}
 
-      buttonDefault();
+function clickClear() {
+  $result = '0';
+  displayResult();
+  console.log($result);
+}
 
-      $(".start").click(function() {
-        startTime = Date.now();
-        countUp();
-        clickButtonStart();
-      }); 
-      
-      $(".stop").click(function() {
-        clearTimeout(timeoutId);
-        elapsedTime += Date.now() - startTime;
-        clickButtonStop();
-      }); 
-      
-      $(".reset").click(function() {
-        $(".timer").text("0:0:0:0");
-        elapsedTime = 0;
-        buttonDefault();
-      }); 
+function clickEqual() {
+  $result = eval($result);
+  displayResult();
+  console.log($result);
+}
 
-
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-  });
+function clickPoint(button) {
+  let number = button.innerHTML;
+  if ($result.slice(-1) === '.') {
+    return;
+  } else if ($result === '0' || $result === '00' || $result === ''){
+    $result = '0';
+    $result += number;
+    displayResult();
+  } else {
+    $result += number;
+    displayResult();
+  }
 }
